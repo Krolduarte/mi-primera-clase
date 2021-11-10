c90c9a6757950daf1e4c66a2cddcede0e7ada7e6
Carol <krolduarte@gmail.com>
2021-11-10 12:47:02 +0100

    Commit inicial

--- ClaseGimnasio.java
+++ ClaseGimnasio.java
@@ -1,9 +1,66 @@
 public class ClaseGimnasio{
-	private String nombreCliente;
-	private int clasesProgramadas;
-	private boolean conReserva;
+    private String nombreCliente;
+    private int clasesProgramadas;
+    private boolean conReserva;
 
-	public ClaseGimnasio(String nombreDelCliente, int numeroDeClases, boolean tieneReserva){
-		nombreCliente = nombreDelCliente;
-		clasesProgramadas = numeroDeClases;
-		Reserva = tieneReserva;
+    public ClaseGimnasio(String nombreDelCliente, int numeroDeClases, boolean tieneReserva){
+        nombreCliente = nombreDelCliente;
+        clasesProgramadas = 0;
+        conReserva = true;
+    }
+        
+    public String getNombre(){
+       return nombreCliente; 
+    }
+    
+    public int getNumeroDeClasesProgramadas(){
+        return clasesProgramadas;
+    }
+    
+    public boolean getEstadoReserva(){
+        return conReserva;
+    }
+    
+    public void setNombreCliente (String nombreDelCliente){
+        nombreCliente = nombreDelCliente;
+        
+    }
+    
+    public void aumentarCantidadClases (int numeroDeClases){
+        clasesProgramadas = numeroDeClases + 1;
+    }
+    
+    public void cambiarEstadoReserva(){
+        conReserva = false;
+    }
+    
+    public void imprimeEstadoReserva(){
+        System.out.println("En el momento, el cliente " + nombreCliente + " tiene reservada " + clasesProgramadas + " clases.");
+    }
+        
+    
+ }
+ 
+ 
+ 
+ 
+ /**
+ 
+ClaseGimnasio
+
+El nombre de los atributos y su tipo de dato.
+
+nombreCliente
+clasesProgramadas
+reserva
+
+El nombre de los métodos modificadores del segundo y tercer atributo.
+
+aumentarCantidadClases
+reservar
+
+El nombre de los dos últimos métodos que se piden.
+
+GetEstadoReserva
+imprimeEstadoReserva
+**/
