734f273c00f74df61cf20b595838ffd55174434e
Carol <krolduarte@gmail.com>
2021-11-10 12:05:43 +0100

    Mi primera clase iniciando a codificar

---
+++ ClaseGimnasio.java
@@ -0,0 +1,9 @@
+public class ClaseGimnasio{
+	private String nombreCliente;
+	private int clasesProgramadas;
+	private boolean conReserva;
+
+	public ClaseGimnasio(String nombreDelCliente, int numeroDeClases, boolean tieneReserva){
+		nombreCliente = nombreDelCliente;
+		clasesProgramadas = numeroDeClases;
+		Reserva = tieneReserva;
